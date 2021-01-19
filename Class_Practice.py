2470f93dc3484b6d323468be2b31709a04b87801
keet <keithprogers@hotmail.ca>
2021-01-19 17:37:41 -0330

    Added some extra classes

--- Class_Practice.py
+++ Class_Practice.py
@@ -16,8 +16,24 @@ class Student:
     def __str__(self):
         return f"{self.firstname} {self.lastname} \nStudent Number: {self.id} \nProgram: {self.programofstudy}"
 
+class Course:
+    """ creating courses for students
+
+    """
+
+    def __init__(self, course_name, max_students):
+        self.course_name = course_name
+        self.max_students = max_students
+        self.students = []
+
+    def add_student(self, student):
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+            return True
+        return False
 
 keet = Student("Keith", "Rogers", 201030756, "Computational Mathematics")
+course = Course("OOP", 2)
+course.add_student(keet)
 
-print(keet)
-keet.complain()
+print(course.students[0].firstname, course.students[0].lastname)

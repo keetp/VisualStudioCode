e713ee829fbd808dc1224618b5331afc34eb2f13
keet <keithprogers@hotmail.ca>
2021-01-19 14:26:17 -0330

    OOP Class #2 Class/Object practice

---
+++ Class_Practice.py
@@ -0,0 +1,16 @@
+class Student:
+
+
+    def __init__(self, firstname, lastname, id, programofstudy):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.id = id
+        self.programofstudy = programofstudy 
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname} \nStudent Number: {self.id} \nProgram: {self.programofstudy} "
+
+
+keet = Student("Keith", "Rogers", 201030756, "Computational Mathematics")
+
+print(keet)

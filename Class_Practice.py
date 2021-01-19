c77626e16ec400cf9381bfa96b30cd0b354e0131
keet <keithprogers@hotmail.ca>
2021-01-19 14:57:57 -0330

    Update #3, mainly formatting/comments

--- Class_Practice.py
+++ Class_Practice.py
@@ -1,5 +1,8 @@
 class Student:
+    """ short class that creates a student object. 
+        done in oop class on Jan 19th 2021
 
+    """
 
     def __init__(self, firstname, lastname, id, programofstudy):
         self.firstname = firstname
@@ -9,7 +12,6 @@ class Student:
 
     def complain(self):
         print("oh man not another assignment")
-
 
     def __str__(self):
         return f"{self.firstname} {self.lastname} \nStudent Number: {self.id} \nProgram: {self.programofstudy}"

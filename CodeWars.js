065225c2b53511a788dbbbecee3cacdae9841cf6
keet <keithprogers@hotmail.ca>
2021-01-20 12:59:56 -0330

    JS practice #1

---
+++ CodeWars.js
@@ -0,0 +1,13 @@
+/* trying to teach myself JavaScript by using the codewars site in combination with freecodecamp
+
+*/
+
+// reverses a given string
+function solution(str){
+    var new_str = '';
+     
+     for(var i = str.length - 1; i >=0; i--){
+         new_str += str[i]
+     }
+     return new_str
+}

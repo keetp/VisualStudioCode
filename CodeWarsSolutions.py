c5e6ce9d3a49ac2bd96cddd4e1fa27f4e0674bf8
keet <keithprogers@hotmail.ca>
2021-01-21 11:16:40 -0330

    linking my git projects to pycharm so I can use pycharm for more dedicated python projects

--- CodeWarsSolutions.py
+++ CodeWarsSolutions.py
@@ -2,26 +2,31 @@
     part of the code wars solutions repository
 
 """
+
+
 # counts vowels in a given string
 
 def get_count(input_str):
     num_vowels = 0
     for ch in input_str:
         if ch in ['a', 'e', 'i', 'o', 'u']:
-            num_vowels+=1   
+            num_vowels += 1
     return num_vowels
 
-import math     # math library
 
-# 0.5 litres drank per hour, returns total litres consumed, always rounded down. 
+import math  # math library
+
+
+# 0.5 litres drank per hour, returns total litres consumed, always rounded down.
 
 def litres(time):
-    return math.floor(time*0.5)
+    return math.floor(time * 0.5)
 
-# squares each digit individually and concatonates them
+
+# squares each digit individually and concatenates them
 
 def square_digits(num):
     new_str = ""
     for dig in str(num):
-        new_str += str(int(dig)**2)
+        new_str += str(int(dig) ** 2)
     return int(new_str)

d6cddbd89b07a1a30220b034e498b3da0b019e06
keet <keithprogers@hotmail.ca>
2021-01-25 12:39:51 -0330

    css, js and html for todo list project updated. still doesnt work

---
+++ todo list project/app.js
@@ -0,0 +1,13 @@
+//Selectors
+const todoInput = document.querySelectorAll('.todo-input');
+const todoButton = document.querySelectorAll('.todo-button');
+const todoList = document.querySelectorAll('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo)
+
+//Functions
+function addTodo(event) {
+    //Prevent form from submitting
+    console.log('hello')
+}

fd00e84fa0d6b82ae372a87b32ff0441c79a8841
keet <keithprogers@hotmail.ca>
2021-01-25 13:29:32 -0330

    additional updates, need to do more research on js to get the webpage working the way i want

--- todo list project/app.js
+++ todo list project/app.js
@@ -1,13 +1,44 @@
-//Selectors
-const todoInput = document.querySelectorAll('.todo-input');
-const todoButton = document.querySelectorAll('.todo-button');
-const todoList = document.querySelectorAll('.todo-list');
+// array to hold the todo list items
+let todoItems = [];
 
-//Event Listeners
-todoButton.addEventListener('click', addTodo)
-
-//Functions
-function addTodo(event) {
-    //Prevent form from submitting
-    console.log('hello')
+// function to render the list items to the webpage for the user
+function renderTodo(todo) {
+    // selecting first element in the list
+    const list = document.querySelector('.todo-list');
+    // need to figure out how to implement this, in over my head
 }
+
+
+
+// function to create a new todo object based on the 
+// text that was entered in the text input, and push it
+// onto the 'todoItems' array
+function addTodo(text){
+    const todo ={
+        text,
+        checked:false,
+        id: Date.now(),
+    };
+
+    todoItems.push(todo);
+    console.log(todoItems)
+    //    renderTodo(todo);
+}
+
+
+// selecting the form element
+const form = document.querySelector('.todo-form');
+// adding a submit event listener
+form.addEventListener('submit', event => {
+    // stops page refresh
+    event.preventDefault();
+    // selecting the text input
+    const input = document.querySelector('.todo-input');
+
+    const text = input.value.trim();
+    if (text !== ''){
+        addTodo(text);
+        input.value='';
+        input.focus();
+    }
+});
